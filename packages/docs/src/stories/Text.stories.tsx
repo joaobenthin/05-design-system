b0ba6d93e24bd13c3d70a2955c6349d9914d6bda
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2023-09-26 11:27:54 -0300

    Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, culpa natus repellendus eveniet voluptatum asperiores laboriosam pariatur dicta. Similique eius doloremque pariatur maxime harum officia assumenda facilis mollitia molestiae corporis.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
